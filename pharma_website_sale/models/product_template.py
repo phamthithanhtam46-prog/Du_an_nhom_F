1fa582198906c86e2f50e31494f1503485c67c15
vntvan <vanvo.31231022154@st.ueh.edu.vn>
2026-05-27 14:43:08 +0700

    Delete pharma_website_sale directory

--- pharma_website_sale/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    prescription_required = fields.Boolean(
-        string='Yêu cầu kê đơn'
-    )
