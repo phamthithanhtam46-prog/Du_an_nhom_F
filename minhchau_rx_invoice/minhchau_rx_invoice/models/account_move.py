16aead8e665f7c79b8830cebc03d76e0f914d54e
linhnguyen31231021750-cmd <linhnguyen.31231021750@st.ueh.edu.vn>
2026-05-30 12:11:26 +0700

    Delete minhchau_rx_invoice/minhchau_rx_invoice directory

--- minhchau_rx_invoice/minhchau_rx_invoice/models/account_move.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class AccountMove(models.Model):
-    _inherit = 'account.move'
-
-    is_rx_prescription = fields.Boolean(string='Là đơn thuốc Rx', readonly=True, copy=False)
-    rx_pharmacist_id = fields.Many2one('res.users', string='Dược sĩ phụ trách', readonly=True, copy=False)
-    is_rx_confirmed = fields.Boolean(string='Đã xác nhận đủ điều kiện bán', readonly=True, copy=False)
-    rx_confirmation_note = fields.Text(string='Ghi chú kiểm tra', readonly=True, copy=False)
